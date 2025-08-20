d4e8a77445dd54bbef844e770fcb7981eeb0128d
Hridoykhan4 <hridoykhan148385@gmail.com>
2025-08-21 00:14:53 +0600

    fruits practiced before project

--- server/index.js
+++ server/index.js
@@ -1,26 +1,58 @@
-
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+// MiddleWare Start
+app.use(cors())
+app.use(express.json())
+
+// MiddleWare End
+
 const uri = "mongodb+srv://hridoykhan148385:SUByXmuSE9Gft4Kk@happiness-hill.1es2bek.mongodb.net/?retryWrites=true&w=majority&appName=Happiness-Hill";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        const roomCollection = client.db('HappinessHill').collection('rooms')
+
+
+        /* Room related APIs start */
+        app.get('/rooms', async (req, res) => {
+            const result = await roomCollection.find().toArray()
+            res.send(result)
+        })
+        /* Room related APIs end */
+
+        
+
+
+
+
+
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    }
 }
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Happiness Hill')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on PORT: ${port}`)
+})
+
 run().catch(console.dir);
